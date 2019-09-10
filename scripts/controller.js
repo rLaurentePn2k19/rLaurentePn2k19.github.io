b3e1b7df4c0082d693f5968561b8ac58602d3a83
Gracejean <jennilyn.mendoza@student.passerellesnumeriques.org>
2019-09-10 18:11:04 +0800

    Initial

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,16 +1,22 @@
 $(document).ready(function () {
-	let list_subscribed = [];
-	let list_published = [];
 	$("body").css({ "height": "100%", "background-color": "black" })
 	$(".card").css({ "height": "100%", "color": "black" })
 	$(".h4").css("color", "black");
 	$("#header_text").css({ "padding": "10px", "border-radius": "0px" })
-	$("img").css("height","35px");
+	$("img").css("height", "35px");
 	//For connect button
-	$('#connect_btn').click(function () {
+	$('#connect_btn').click(function (e) {
+		e.preventDefault();
 		var connect = $("#broker_input").val();
 		client = mqtt.connect(connect);
-		client.subscribe($("#topic").val());
+		// client.subscribe($("#topic").val());
+		client.on("message", function (topic, payload) {
+			var row = $("<tr>");
+			$("<td>").text(topic).appendTo($(row));
+			$("<td>").text(payload).appendTo($(row));
+			$("<td>").text(moment().format('MMMM Do YYYY, h:mm:ss a')).appendTo($(row));
+			$("#tbl-body").append($(row));
+		})
 		console.log('Connecting..');
 		$("#display_status").text("Connecting");
 		$("#display_status").removeClass("alert-secondary");
@@ -28,173 +34,138 @@ $(document).ready(function () {
 			})
 			console.log("Successfully Connected.");
 		});
-		//For Disconnect button
-		$(".disconnect_btn").click(function () {
-			Swal.fire({
-				title: 'Are you sure?',
-				text: "You won't be undo this",
-				type: 'warning',
-				showCancelButton: true,
-				confirmButtonColor: '#3085d6',
-				cancelButtonColor: '#d33',
-				confirmButtonText: 'Yes, disconnect!'
-			}).then((result) => {
-				if (result.value) {
-					client.end();
-					Swal.fire(
-						'Disconnected!',
-						'Your are now disconnected to the broker!',
-						'success'
-					);
-					$("#display_status").text("Disconnected");
-					$("#display_status").removeClass("aalert alert-success");
-					$("#display_status").addClass("alert-secondary");
-				}
-			})
-		});
-		//For Publish 
-		$("#publish_btn").click(function () {
-			var topic = $("#topic").val();
-			var payload = $("#payload").val();
-			if (topic == "" && payload == "") {
-				client.publish("", "");
-				Swal.fire({
-					type: 'error',
-					title: 'Oops...',
-					text: 'Please provide inputs!',
-					animation: false,
-					customClass: {
-						popup: 'animated tada'
-					}
-				});
-			}else if(list_published.includes(topic)){
-				Swal.fire({
-					type: 'error',
-					title: 'Oops...',
-					text: 'You already published this topic!',
-					animation: false,
-					customClass: {
-						popup: 'animated tada'
-					}
-				});
-			}
-			else {
-				client.publish(topic, payload, function (err) {
-					if (err) {
-						Swal.fire({
-							type: 'error',
-							title: 'Oops...',
-							text: 'An error occurs!',
-							animation: false,
-							customClass: {
-								popup: 'animated tada'
-							}
-						});
-					} else {
-						list_published.push(topic)
-						console.log("Publish { Topic: " + $("#topic").val() + ","+" Payload: "+ $("#payload").val() + "}");
-						Swal.fire({
-							type: 'success',
-							title: 'Published successfully!',
-							timer: 2500,
-							animation: true
-						});
-					}
-					console.log("Succesfully Published")
-					var row = $("<tr>");
-					$("<td>").text(topic).appendTo($(row));
-					$("<td>").text(payload).appendTo($(row));
-					$("<td>").text(moment().format('MMMM Do YYYY, h:mm:ss a')).appendTo($(row));
-					$("#tbl-body-pub").append($(row));
-				});
-			}
-		});
 		
-		//For Subscribe
-		$("#subscribe_btn").click(function () {
-			var subscribe = $("#topic-sub").val();
-			var topic = $("#topic").val();
-			if (subscribe != topic) {
-				Swal.fire({
-					type: 'error',
-					title: 'Oops...',
-					text: 'Topic is not available!',
-					animation: false,
-					customClass: {
-						popup: 'animated tada'
-					}
-				});
-			}else if(list_subscribed.includes(subscribe)){
-				Swal.fire({
-					type: 'error',
-					title: 'Oops...',
-					text: 'You already subscribed this topic!',
-					animation: false,
-					customClass: {
-						popup: 'animated tada'
-					}
-				});
-			}
-			else if (subscribe == topic && topic !== "") {
-				client.subscribe(topic, function (err) {
-					if (err) {
-						Swal.fire({
-							type: 'error',
-							title: 'Oops...',
-							text: 'An error occurs!',
-							animation: false,
-							customClass: {
-								popup: 'animated tada'
-							}
-						});
-					} else {
-						list_subscribed.push(subscribe);
-						var row = $("<tr>").attr("id", "mysub");
-						$("<td>").text(topic).appendTo($(row));
-						$("<td>").text(moment().format('MMMM Do YYYY, h:mm:ss a')).appendTo($(row));
-						$("#tbl-body-sub").append($(row))
-  					console.log("Subscribe { Topic: " + $("#topic-sub").val() + " }");
-						Swal.fire({
-							title: 'Subscribed successfully!',
-							animation: true,
-							timer: 2500,
-						});
-						console.log("Succesfully Subscribed ");
-					}
-				});
+	});
+
+
+	//For Disconnect button
+	$(".disconnect_btn").click(function (e) {
+		e.preventDefault();
+		Swal.fire({
+			title: 'Are you sure?',
+			text: "You won't be undo this",
+			type: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Yes, disconnect!'
+		}).then((result) => {
+			if (result.value) {
+				client.end();
+				Swal.fire(
+					'Disconnected!',
+					'Your are now disconnected to the broker!',
+					'success'
+				);
+				$("#display_status").text("Disconnected");
+				$("#display_status").removeClass("aalert alert-success");
+				$("#display_status").addClass("alert-secondary");
 			}
 		})
-		//For Unsubscribe
-		$("#unsubscribe_btn").click(function () {
-			var subscribe = $("#topic-sub").val();
-			if(list_subscribed.length == 0){
+	});
+
+	//For Publish 
+	$("#publish_btn").click(function (e) {
+		e.preventDefault();
+		var topic = $("#topic").val();
+		var payload = $("#payload").val();
+		if (topic == "" && payload == "") {
+			client.publish("", "");
+			Swal.fire({
+				type: 'error',
+				title: 'Oops...',
+				text: 'Please provide inputs!',
+				animation: false,
+				customClass: {
+					popup: 'animated tada'
+				}
+			});
+		} else {
+			client.publish(topic, payload, function (err) {
+				if (err) {
+					Swal.fire({
+						type: 'error',
+						title: 'Oops...',
+						text: 'An error occurs!',
+						animation: false,
+						customClass: {
+							popup: 'animated tada'
+						}
+					});
+				} else {
+					Swal.fire({
+						type: 'success',
+						title: 'Published successfully!',
+						timer: 2500,
+						animation: true
+					});
+				}
+				var row = $("<tr>");
+				$("<td>").text(topic).appendTo($(row));
+				$("<td>").text(payload).appendTo($(row));
+				$("<td>").text(moment().format('MMMM Do YYYY, h:mm:ss a')).appendTo($(row));
+				$("#tbl-body-pub").append($(row));
+				console.log("Publish { Topic: " + $("#topic").val() + "," + " Payload: " + $("#payload").val() + "}");
+				console.log("Succesfully Published")
+			});
+		}
+	});
+
+	//For Subscribe
+	$("#subscribe_btn").click(function (e) {
+		client.subscribe($("#topic-sub").val());
+
+		e.preventDefault();
+		var subscribe = $("#topic-sub").val();
+		var topic = $("#topic").val();
+		if (subscribe == "") {
+			client.subscribe("", "")
+			Swal.fire({
+				type: 'error',
+				title: 'Oops...',
+				text: 'Invalid Topic!.',
+				animation: false,
+				customClass: {
+					popup: 'animated tada'
+				}
+			});
+		}
+		client.subscribe(topic, function (err) {
+			if (err) {
 				Swal.fire({
 					type: 'error',
 					title: 'Oops...',
-					text: 'You cannot do this without subscribing first.',
+					text: 'An error occurs!',
 					animation: false,
 					customClass: {
 						popup: 'animated tada'
 					}
 				});
-			}else{
-				$("#topic-sub").val("");
-				$("#mysub").remove();
-				list_subscribed.splice(subscribe)
+			} else {
+				var row = $("<tr>").attr("id", "mysub");
+				$("<td>").text(subscribe).appendTo($(row));
+				$("<td>").text(moment().format('MMMM Do YYYY, h:mm:ss a')).appendTo($(row));
+				$("#tbl-body-sub").append($(row))
+				console.log("Subscribe { Topic: " + $("#topic-sub").val() + " }");
 				Swal.fire({
-					title: 'Unsubrscribed successfully!',
+					title: 'Subscribed successfully!',
 					animation: true,
 					timer: 2500,
 				});
+				console.log("Succesfully Subscribed ");
 			}
-			console.log("Successfully Unsubscribed ");
-		})
-		// client.on("message", function (topic, payload) {
-		// 	var row = $("<tr>");
-		// 	$("<td>").text(topic).appendTo($(row));
-		// 	$("<td>").text(payload).appendTo($(row));
-		// 	$("<td>").text(moment().format('MMMM Do YYYY, h:mm:ss a')).appendTo($(row));
-		// 	$("#tbl-body").append($(row));
-		// })
-	});
-})
+		});
+	})
+	//For Unsubscribe
+	$("#unsubscribe_btn").click(function (e) {
+		e.preventDefault();
+		$("#topic-sub").val("");
+		$("#mysub").remove();
+		Swal.fire({
+			title: 'Unsubrscribed successfully!',
+			animation: true,
+			timer: 2500,
+		});
+		console.log("Successfully Unsubscribed ");
+	})
+});
